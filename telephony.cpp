0767fb525bd40b21b635e3f8a821f225c89416d0
TNK <jura.hlebin@gmail.com>
2025-07-01 21:04:51 +0300

    feature(core): added settings reconfiguring

--- telephony.cpp
+++ telephony.cpp
@@ -2,17 +2,54 @@
 #include <fstream>
 #include <filesystem>
 
+std::string addressAPI = "Address=";
+std::string unique = "Unique=";
+std::string key = "Key=";
+std::string port = "Port=";
 
+
+void reconfigureSettings()
+{
+    std::string addressAPIvalue;
+    std::string uniqueValue;
+    std::string keyValue;
+    std::string portValue;
+
+    std::cout << "Write API adress: ";
+    std::cin >> addressAPIvalue;
+    addressAPIvalue = addressAPI + addressAPIvalue +"\n";
+
+    std::cout << "Write unique: ";
+    std::cin >> uniqueValue;
+    uniqueValue = unique + uniqueValue +"\n";
+
+    std::cout << "Write key: ";
+    std::cin >> keyValue;
+    keyValue = key + keyValue+"\n";
+
+    std::cout << "Write port: ";
+    std::cin >> portValue;
+    portValue = port + portValue;
+
+    std::ofstream file ("connectionSettings.txt");
+    file << addressAPIvalue;
+    file << uniqueValue;
+    file << keyValue;
+    file << portValue;
+
+}
 
 void readSettings()
 {
     if (std::filesystem::exists("/home/mainuser/projects/telephony/connectionSettings.txt"))
     {
-        std::cout << "file exists";
+        std::cout << "File exists";
     }
     else
     {
-        std::ofstream file ("connectionSettings.txt");
+        std::cout << "File not found, configuring settings";
+        reconfigureSettings();
+        
     }
 }
 int main()
